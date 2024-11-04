958fd1664b0a8666695d802cef77d5dad4f0f8dd
jjjch0723 <83578340+jjjch0723@users.noreply.github.com>
2024-11-05 02:28:42 +0900

    teamInfo, playerInfo

--- app/api/v1/endpoints/getTeamsInfo.py
+++ app/api/v1/endpoints/getTeamsInfo.py
@@ -3,6 +3,7 @@ from fastapi.responses import JSONResponse
 from app.db.connection import get_connection
 from app.core.config import settings
 from app.db.queries import teamsInfo
+from app.db.queries import teamInfo as ti
 import logging
 
 logger = logging.getLogger(__name__)
@@ -32,3 +33,26 @@ async def get_teams():
     except Exception as e:
         logger.error(f"Error fetching teams: {str(e)}")
         raise HTTPException(status_code=500, detail="An error occurred while fetching teams.")
+
+
+@router.get("/teamInfo", response_model=dict)
+async def get_teamInfo(teamcode : str):
+    try:
+        with get_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute(ti, (teamcode, ))
+            team = cursor.fetchone()
+
+        if not team:
+            return JSONResponse(status_code=404, content={"message": "Team not found!"})
+
+        result = {
+            "리그명": team[0],
+            "팀명": team[1],
+            "영어팀명": team[2],
+            "과거팀명": team[3]
+        }
+        return {"team": result}
+    except Exception as e:
+        logger.error(f"Error fetching team info for teamcode {teamcode}: {str(e)}")
+        raise HTTPException(status_code=500, detail="An error occurred while fetching team information.")
--- app/db/queries.py
+++ app/db/queries.py
@@ -7,3 +7,26 @@ select ttm.league_code as "리그명", ttm.teamname_kr as "팀명", ttm.teamname
   end as "과거팀명"
   from tbl_team_mt01 ttm 
 """
+
+teamInfo = """
+select ttm.league_code as "리그명", ttm.teamname_kr as "팀명", ttm.teamname_us as "영어팀명",
+  case
+	when ttm.cur_teamname_kr is null and ttm.cur_teamname_us is null
+    then 'There''s no current teamname'
+    else CONCAT(ttm.cur_teamname_kr, ' / ', ttm.cur_teamname_us) 
+  end as "과거팀명"
+  from tbl_team_mt01 ttm 
+ where ttm.team_code = %s
+"""
+
+players ="""
+select tmn.fullname, tmn."position", tmn.teamname,  
+  case 
+  	when tmn.league_code = 'AL'
+  	then 'American League'
+  	when tmn.league_code = 'NL'
+  	then 'National Leauge'
+  end as league_name
+  from tbl_mlbplayer_nt01 tmn 
+ where tmn.teamcode = %s
+"""
