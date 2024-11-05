54d58aa18ecf97220c6b902edaf1aa513cc128bf
jjjch0723 <83578340+jjjch0723@users.noreply.github.com>
2024-11-05 21:59:03 +0900

    roster
    26인 로스터 제공

--- app/api/v1/endpoints/getTeamsInfo.py
+++ app/api/v1/endpoints/getTeamsInfo.py
@@ -5,6 +5,7 @@ from app.core.config import settings
 from app.db.queries import teamsInfo
 from app.db.queries import teamInfo as ti
 from app.db.queries import position
+from app.db.queries import players
 import logging
 
 logger = logging.getLogger(__name__)
@@ -83,3 +84,30 @@ async def get_positionInfo(pos : str):
     except Exception as e:
         logger.error(f"Error fetching team info for position {pos}: {str(e)}")
         raise HTTPException(status_code=500, detail="An error occurred while fetching position information.")
+    
+@router.get("/roster", response_model=dict)
+async def get_roster(teamcode : str):
+    try:
+        with get_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute(players, (teamcode, ))
+            roster = cursor.fetchall()
+
+        if not roster:
+            return JSONResponse(status_code=404, content={"message": "No players"})
+
+        result = [
+            {
+                "리그명": row[0],
+                "포지션": row[1],
+                "이름": row[2],
+                "팀명": row[3],
+                "팀코드": row[4]
+            }
+            for row in roster
+        ]
+        return {"roster": result}
+    except Exception as e:
+        logger.error(f"Error fetching team info for position {teamcode}: {str(e)}")
+        raise HTTPException(status_code=500, detail="An error occurred while fetching position information.")
+    
--- app/db/queries.py
+++ app/db/queries.py
@@ -1,3 +1,4 @@
+# 전체 팀정보
 teamsInfo = """
 select ttm.league_code as "리그명", ttm.teamname_kr as "팀명", ttm.teamname_us as "영어팀명", 
   case
@@ -8,6 +9,7 @@ select ttm.league_code as "리그명", ttm.teamname_kr as "팀명", ttm.teamname
   from tbl_team_mt01 ttm 
 """
 
+# 특정 팀정보
 teamInfo = """
 select ttm.league_code as "리그명", ttm.teamname_kr as "팀명", ttm.teamname_us as "영어팀명",
   case
@@ -19,18 +21,21 @@ select ttm.league_code as "리그명", ttm.teamname_kr as "팀명", ttm.teamname
  where ttm.team_code = %s
 """
 
+# 팀 로스터 정보
 players ="""
-select tmn.fullname, tmn."position", tmn.teamname,  
+select 
   case 
   	when tmn.league_code = 'AL'
   	then 'American League'
   	when tmn.league_code = 'NL'
   	then 'National Leauge'
-  end as league_name
+   end as league_name, tmn.teamname, tmn.fullname, tmn."position", tmn.teamcode
   from tbl_mlbplayer_nt01 tmn 
  where tmn.teamcode = %s
+ order by "position" 
 """
 
+#포지션별 선수 모음
 position ="""
 select
   case 
@@ -41,4 +46,5 @@ select
   end as league_name, tmn."position", tmn.fullname, tmn.teamname, tmn.teamcode
   from tbl_mlbplayer_nt01 tmn 
  where tmn."position" = %s
+ order by tmn.teamcode
 """
